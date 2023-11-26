2cbbe0effacce716be697186b72b836ffc3f68bf
YoYomec <yoanncrouzet10@outlook.fr>
2023-11-26 13:39:19 +0100

    voila

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
-import StudentsEditor from './components/ClassEditor.vue';
-import Input from './components/Input.vue';
-import TeamsViewer from './components/TeamsViewer.vue';
-import { useMainStore } from './stores/main';
+import ClassesEditor from "./components/ClassesEditor.vue";
+import TeamsViewer from "./components/viewers/TeamsViewer.vue";
+import { useMainStore } from "./stores/main";
 
-
-const mainStore = useMainStore()
+const mainStore = useMainStore();
 </script>
 
 <template>
-    <header>
-        <button @click="mainStore.generateTeams()" style="margin-bottom: var(--spacing); width: 100%;">Génerer les équipes</button>
-    </header>
-    <div style="display: flex; flex-direction: row; gap: 6px; flex-wrap: wrap;">
-        <Input style="flex-shrink: 1; flex-grow: 1;" />
-        <TeamsViewer />
-    </div>
+	<header>
+		<button
+			@click="mainStore.generateTeams()"
+			style="margin-bottom: var(--spacing); width: 100%"
+		>
+			Génerer les équipes
+		</button>
+	</header>
+	<div style="display: flex; flex-direction: row; gap: 6px; flex-wrap: wrap">
+		<ClassesEditor style="flex-shrink: 1; flex-grow: 1" />
+		<TeamsViewer />
+	</div>
 </template>
---
+++ src/components/ClassesEditor.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+import { useMainStore } from "@/stores/main";
+import { computed, ref, type Ref } from "vue";
+
+const mainStore = useMainStore();
+const savingClassKey = ref(mainStore.currentClass);
+const savingClassExist = computed(
+	() => savingClassKey.value.replace(" ", "") !== ""
+);
+
+function stringToList(text: string) {
+	return text.split("\n");
+}
+
+function listToString(list: string[]) {
+	return list.join("\n");
+}
+</script>
+
+<template>
+	<fieldset>
+		<select v-model="mainStore.currentClass">
+			<option
+				v-for="classKey of Object.keys(mainStore.classes)"
+				:value="classKey"
+			>
+				{{ classKey }}
+			</option>
+		</select>
+		<button
+			@click="mainStore.deleteClass(mainStore.currentClass)"
+			:disabled="!mainStore.canDeleteClass(mainStore.currentClass)"
+		>
+			Supprimer la classe
+		</button>
+
+		<div
+			style="
+				display: flex;
+				flex-direction: row;
+				gap: var(--spacing);
+				flex-wrap: wrap;
+				width: 100%;
+			"
+		>
+			<textarea
+				style="height: 325px; flex-grow: 1; width: 150px"
+				v-for="(students, i) of mainStore.students"
+				:value="listToString(mainStore.students[i])"
+				@input="
+					mainStore.students[i] = stringToList(
+						($event.target as HTMLInputElement).value
+					)
+				"
+			></textarea>
+		</div>
+
+		<label for="save-class-name">Nom de la classe à sauvegarder :</label>
+		<input
+			type="text"
+			id="save-class-name"
+			v-model="savingClassKey"
+			placeholder="Nom de la classe"
+			style="max-width: 125px"
+		/>
+		<button
+			@click="mainStore.saveClass(savingClassKey, mainStore.students)"
+			name="Sauvegarder"
+			data-tooltip="Savegarder la classe en choisisant un nom, ou modifier une classe existante"
+		>
+			Sauvegarder
+		</button>
+	</fieldset>
+</template>
